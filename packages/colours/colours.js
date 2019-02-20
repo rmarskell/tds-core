9a466c0e8e14c8da34c522634cbe9b4ae63f5ded
Jordan Raffoul <me@jordanraffoul.com>
2019-02-20 16:08:59 -0500

    feat(core-colours): add colour object

---
+++ packages/colours/colours.js
@@ -0,0 +1,30 @@
+export default {
+  accessibleGreen: '#248700',
+
+  telusGreen: '#6c0',
+  panache: '#f4f9f2',
+
+  shuttleGrey: '#54595f',
+
+  shark: '#2a2c2e',
+  athensGrey: '#f7f7f8',
+
+  telusPurple: '#4b286d',
+
+  whiteLilac: '#f2eff4',
+  cardinal: '#c12335',
+  lavenderBlush: '#fff6f8',
+  white: '#fff',
+  gainsboro: '#d8d8d8',
+
+  link: '#54595f',
+  text: '#2a2c2e',
+
+  primary: '#248700',
+  secondary: '#4b286d',
+
+  iconPrimary: '#248700',
+  iconSecondary: '#4b286d',
+  iconDisabled: '#54595f',
+  iconError: '#c12335',
+}
