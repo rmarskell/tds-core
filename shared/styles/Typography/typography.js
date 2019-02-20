26264b4bcf4e71bfbeaf421297febfc60c0a9375
Jordan Raffoul <me@jordanraffoul.com>
2019-02-20 16:06:54 -0500

    feat(shared-typography): add typography style object mixins

---
+++ shared/styles/Typography/typography.js
@@ -0,0 +1,37 @@
+export default {
+  helveticaNeueThin35: {
+    fontWeight: 300,
+  },
+
+  helveticaNeueLight45: {
+    fontWeight: 400,
+  },
+
+  helveticaNeueRoman55: {
+    fontWeight: 500,
+  },
+
+  helveticaNeueMedium65: {
+    fontWeight: 700,
+  },
+
+  sizes: {
+    small: {
+      fontSize: '0.875rem',
+      letterSpacing: -0.6,
+      lineHeight: 1.42857,
+    },
+
+    medium: {
+      fontSize: '1rem',
+      letterSpacing: -0.8,
+      lineHeight: 1.5,
+    },
+
+    large: {
+      fontSize: '1.25rem',
+      letterSpacing: -1,
+      lineHeight: 1.6,
+    },
+  },
+}
